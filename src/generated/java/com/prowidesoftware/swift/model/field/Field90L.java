c134fea0c7adc198166b4d338de2a65e59c845b2
zubri <sebastian@prowidesoftware.com>
2021-04-24 16:16:14 -0300

    global copyright comment update

--- src/generated/java/com/prowidesoftware/swift/model/field/Field90L.java
+++ src/generated/java/com/prowidesoftware/swift/model/field/Field90L.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2006-2020 Prowide
+ * Copyright 2006-2021 Prowide
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
