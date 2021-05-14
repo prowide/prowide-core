e80312a5019794f40a3c29db9318f68ee99bc5ed
zubri <sebastian@prowidesoftware.com>
2021-05-14 19:25:59 -0300

    simple field name

---
+++ src/test/java/com/prowidesoftware/swift/model/field/Field45BTest.java
@@ -0,0 +1,33 @@
+/*
+ * Copyright 2006-2021 Prowide
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.prowidesoftware.swift.model.field;
+
+import com.prowidesoftware.swift.model.Tag;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class Field45BTest {
+
+    @Test
+    public void testAsTag() {
+        Field45B field = new Field45B("/INSTR/Narrative text");
+        Tag tag = field.asTag();
+        assertEquals(Field45B.NAME, tag.getName());
+        assertEquals("/INSTR/Narrative text", tag.getValue());
+    }
+
+}
