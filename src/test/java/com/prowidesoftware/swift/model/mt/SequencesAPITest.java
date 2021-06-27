06351abe46fe1d73219e1cfc803228f8031fb62e
zubri <sebastian@prowidesoftware.com>
2021-06-27 00:08:44 -0300

    sequence API test case

--- src/test/java/com/prowidesoftware/swift/model/mt/SequencesAPITest.java
+++ src/test/java/com/prowidesoftware/swift/model/mt/SequencesAPITest.java
@@ -31,16 +31,10 @@ import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-/**
- * Main test for MT sequences API
- *
- * @author sebastian
- * @since 7.7
- */
 public class SequencesAPITest {
 
     @Test
-    public void test_01() {
+    public void testMT564_SequenceEList() {
         final MT564 m = new MT564();
         m.append(new Tag(Field16R.NAME, MT564.SequenceE.START_END_16RS));
         m.append(new Tag(Field16S.NAME, MT564.SequenceE.START_END_16RS));
@@ -49,7 +43,7 @@ public class SequencesAPITest {
     }
 
     @Test
-    public void test_02() {
+    public void testMT564_SequenceEList_2() {
         final MT564 m = new MT564();
         m.append(new Tag(Field16R.NAME, MT564.SequenceE.START_END_16RS));
         m.append(new Tag(Field16S.NAME, MT564.SequenceE.START_END_16RS));
@@ -60,7 +54,7 @@ public class SequencesAPITest {
     }
 
     @Test
-    public void test_03() {
+    public void testMT564_SequenceEList_3() {
         final MT564 m = new MT564();
         m.append(new Tag(Field16R.NAME, MT564.SequenceE.START_END_16RS));
         m.append(new Tag(Field16R.NAME, MT564.SequenceE1.START_END_16RS));
@@ -73,7 +67,7 @@ public class SequencesAPITest {
     }
 
     @Test
-    public void test_04() {
+    public void testMT564_SequenceEList_4() {
         final MT564 m = new MT564();
         m.append(new Tag(Field16R.NAME, MT564.SequenceE.START_END_16RS));
         m.append(new Tag(Field16R.NAME, MT564.SequenceE1.START_END_16RS));
@@ -88,7 +82,7 @@ public class SequencesAPITest {
     }
 
     @Test
-    public void test_05() {
+    public void testMT564_SequenceEList_5() {
         final MT564 m = new MT564();
         m.append(new Tag(Field16R.NAME, MT564.SequenceE.START_END_16RS));
         m.append(new Tag(Field16R.NAME, MT564.SequenceE1.START_END_16RS));
@@ -105,7 +99,7 @@ public class SequencesAPITest {
     }
 
     @Test
-    public void test_06() {
+    public void testMT564_SequenceEList_6() {
         final MT564 m = new MT564();
         m.append(new Tag(Field16R.NAME, MT564.SequenceE.START_END_16RS));
         m.append(new Tag(Field16S.NAME, MT564.SequenceE.START_END_16RS));
@@ -125,7 +119,7 @@ public class SequencesAPITest {
     }
 
     @Test
-    public void test_07() {
+    public void testMT564_SequenceC() {
         final String s =
                 "{1:F01SWHQBEBBAXXX0001000001}{2:I564SWHQBEBBXBILN}{3:{108:495}}{4:\n" +
                         ":16R:GENL\n" +
@@ -140,8 +134,8 @@ public class SequencesAPITest {
 
                         ":16R:USECU\n" +
                         ":35B:ISIN AT0000A00GJ3\n" +
-                        "FOO 322 Euro\n"
-                        + " FOO Duration(T)\n" +
+                        "FOO 322 Euro\n" +
+                        " FOO Duration(T)\n" +
                         ":16R:ACCTINFO\n" +
                         ":97C::SAFE//GENR\n" +
                         ":16S:ACCTINFO\n" +
@@ -153,19 +147,66 @@ public class SequencesAPITest {
                         ":16S:CADETL\n" +
 
                         ":16R:ADDINFO\n" +
-                        ":70E::ADTX//Fondsfusion\n"
-                        + " FOO 322 Euro \n"
-                        + "FOO Duration\n" +
-                        ":70E::TXNR//Raiffeisen \n"
-                        + "Kapitalanlage Gesellschaft\n"
-                        + " m.b.H. informiert gem.\n"
-                        + " Paragraph133 Abs. 1 InvFG\n"
-                        + " 2011, dass\n" +
+                        ":70E::ADTX//Fondsfusion\n" +
+                        " FOO 322 Euro \n" +
+                        "FOO Duration\n" +
+                        ":70E::TXNR//Foobar \n" +
+                        "Kapitalanlage Gesellschaft\n" +
+                        " m.b.H. informiert gem.\n" +
+                        " Paragraph133 Abs. 9 InvFG\n" +
+                        " 2011, dass\n" +
                         ":16S:ADDINFO\n" +
                         "-}";
         final MT564 m = new MT564(s);
         assertEquals(0, m.getSequenceC().size());
         assertEquals(0, MT564.getSequenceC(m.getSwiftMessage().getBlock4()).size());
+    }
+
+    @Test
+    public void testMT564_SequenceB1() {
+        MT564 mt = MT564.parse("{1:F01SAMPLEXXXXXX0300000054}{2:O5641738190122EDISGB2LAXXX12345123451901230111N}{4:\n" +
+                ":16R:GENL\n" +
+                ":20C::CORP//VQ8000V1LOI51000\n" +
+                ":20C::SEME//VQ8000V1LOI51000\n" +
+                ":23G:NEWM\n" +
+                ":22F::CAEV//ACTV\n" +
+                ":22F::CAMV//MAND\n" +
+                ":25D::PROC//COMP\n" +
+                ":16S:GENL\n" +
+
+                // B1 not present in USECU (B)
+                ":16R:USECU\n" +
+                ":35B:ISIN INE411H01032\n" +
+                ":16R:ACCTINFO\n" +
+                ":97C::SAFE//GENR\n" +
+                ":16S:ACCTINFO\n" +
+                ":16S:USECU\n" +
+
+                ":16R:CAOPTN\n" +
+                ":13A::CAON//001\n" +
+                ":22F::CAOP//CASH\n" +
+                ":17B::DFLT//Y\n" +
+
+                ":16R:SECMOVE\n" +
+                ":22H::CRDB//CRED\n" +
+                ":35B:ISIN XXXXXXXX91B9\n" +
+
+                // colliding FIA sequence in SECMOVE
+                ":16R:FIA\n" +
+                ":92A::PRFC//N0,1\n" +
+                ":16S:FIA\n" +
+
+                ":98A::PAYD//20250329\n" +
+                ":16S:SECMOVE\n" +
+
+                ":16S:CAOPTN\n" +
+                "-}");
+        MT564.SequenceB seqB = mt.getSequenceB();
+        assertNotNull(seqB);
+        assertEquals(6, seqB.size());
+        assertTrue(mt.getSequenceB1().isEmpty());
+
+        assertTrue(mt.getSequenceB1(mt.getSwiftMessage().getBlock4()).isEmpty());
     }
 
     @Test
@@ -179,7 +220,7 @@ public class SequencesAPITest {
     }
 
     @Test
-    public void test670_SequenceC() throws Exception {
+    public void testMT670_SequenceC() {
         MT670 m = new MT670()
                 .append(MT670.SequenceA2.newInstance(
                         Field95P.tag("SSIR")
@@ -194,7 +235,7 @@ public class SequencesAPITest {
     }
 
     @Test
-    public void testMT360() throws Exception {
+    public void testMT360() {
         MT360 m = new MT360()
                 .append(MT360.SequenceA.newInstance(Field23A.tag("FIXEDFIXED")))
                 .append(MT360.SequenceB.newInstance())
@@ -204,7 +245,7 @@ public class SequencesAPITest {
     }
 
     @Test
-    public void testNewSequenceA_MT101() throws Exception {
+    public void testMT101_NewSequenceA() {
         MT101 m = new MT101().append(MT101.SequenceA.newInstance(Field98A.emptyTag()));
         SequenceA A = m.getSequenceA();
         assertNotNull(A);
@@ -212,14 +253,14 @@ public class SequencesAPITest {
     }
 
     @Test
-    public void testNewSequenceB_MT101() throws Exception {
+    public void testMT101_NewSequenceB() {
         MT101 m = new MT101().append(MT101.SequenceB.newInstance(Field98A.emptyTag()));
         List<SequenceB> Bs = m.getSequenceBList();
         assertEquals(1, Bs.size());
     }
 
     @Test
-    public void test104_SequenceC() throws Exception {
+    public void testMT104_SequenceC() {
         MT104 m = new MT104()
                 .append(MT104.SequenceA.newInstance(
                         Field20.tag("FOO"),
@@ -237,4 +278,5 @@ public class SequencesAPITest {
         assertEquals(Field32B.NAME, m.getSequenceC().getTag(0).getName());
         assertEquals(Field71G.NAME, m.getSequenceC().getTag(1).getName());
     }
+
 }
