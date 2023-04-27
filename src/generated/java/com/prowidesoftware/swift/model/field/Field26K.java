453ccbeaadfd0d4c085a0905a236595ca7a9ee39
ptorres-prowide <pablo.torres@prowidesoftware.com>
2023-04-27 12:51:26 -0300

    codegenMtFields

--- src/generated/java/com/prowidesoftware/swift/model/field/Field26K.java
+++ src/generated/java/com/prowidesoftware/swift/model/field/Field26K.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2006-2022 Prowide
+ * Copyright 2006-2023 Prowide
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -57,7 +57,7 @@ import com.google.gson.JsonParser;
  * </ul>
  *
  * <p>
- * This class complies with standard release <strong>SRU2022</strong>
+ * This class complies with standard release <strong>SRU2023</strong>
  */
 @SuppressWarnings("unused")
 @Generated
@@ -65,7 +65,7 @@ public class Field26K extends Field implements Serializable {
 	/**
 	 * Constant identifying the SRU to which this class belongs to.
 	 */
-	public static final int SRU = 2022;
+	public static final int SRU = 2023;
 
 	private static final long serialVersionUID = 1L;
 	/**
