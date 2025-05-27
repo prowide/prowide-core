541ab45b39905de853b30ed8734e384c11dec1a5
fernandodesarriera <fernando@prowidesoftware.com>
2025-05-27 11:25:18 +0200

    Add uetr as a new property of the AbstractSwiftMessage

--- src/main/java/com/prowidesoftware/swift/model/AbstractSwiftMessage.java
+++ src/main/java/com/prowidesoftware/swift/model/AbstractSwiftMessage.java
@@ -154,6 +154,12 @@ public abstract class AbstractSwiftMessage implements Serializable, JsonSerializ
     private java.util.Calendar tradeDate;
 
     /**
+     * @since 10.2.7
+     */
+    @Column(length = 36)
+    private String uetr;
+
+    /**
      * Empty constructor provided for the ORM only, use one of the specific constructors instead.
      *
      * @since 7.7
@@ -1425,5 +1431,22 @@ public abstract class AbstractSwiftMessage implements Serializable, JsonSerializ
         } else {
             return getIdentifier();
         }
+    }
+
+    /**
+     *
+     * @return the Unique End-to-End Transaction Reference (UETR) for this message, if applicable.
+     * @since 10.2.7
+     */
+    public String getUetr() {
+        return uetr;
+    }
+
+    /**
+     * @param uetr the Unique End-to-End Transaction Reference (UETR) for this message, if applicable.
+     * @since 10.2.7
+     */
+    public void setUetr(String uetr) {
+        this.uetr = uetr;
     }
 }
